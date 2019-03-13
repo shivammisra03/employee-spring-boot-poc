382b19add727e2860ef3fbc75486084960c83355
Shivam Misra <shivam.misra03@gmail.com>
2019-03-13 19:03:20 +0530

    Added logic to return a List<Employee> instead of List<Object[]>, and junits for all the methods

--- employee-jpa/src/main/java/com/poc/employeejpa/entity/Employee.java
+++ employee-jpa/src/main/java/com/poc/employeejpa/entity/Employee.java
@@ -11,13 +11,13 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "employees")
 @NamedStoredProcedureQueries({
-		@NamedStoredProcedureQuery(name = "getAllEmployees", procedureName = "get_all_employees"),
-		@NamedStoredProcedureQuery(name = "getEmpById", procedureName = "get_emp_by_id", parameters = {
+		@NamedStoredProcedureQuery(name = "getAllEmployees", procedureName = "get_all_employees", resultClasses = Employee.class),
+		@NamedStoredProcedureQuery(name = "getEmpById", procedureName = "get_emp_by_id", resultClasses = Employee.class, parameters = {
 				@StoredProcedureParameter(mode = ParameterMode.IN, name = "eid", type = Integer.class) }),
-		@NamedStoredProcedureQuery(name = "getEmpByIdAndSal", procedureName = "get_emp_using_id_salary", parameters = {
+		@NamedStoredProcedureQuery(name = "getEmpByIdAndSal", procedureName = "get_emp_using_id_salary", resultClasses = Employee.class, parameters = {
 				@StoredProcedureParameter(mode = ParameterMode.IN, name = "eid", type = Integer.class),
 				@StoredProcedureParameter(mode = ParameterMode.IN, name = "esalary", type = Integer.class) }),
-		@NamedStoredProcedureQuery(name = "getNameById", procedureName = "get_name_by_id", parameters = {
+		@NamedStoredProcedureQuery(name = "getNameById", procedureName = "get_name_by_id", resultClasses = Employee.class, parameters = {
 				@StoredProcedureParameter(mode = ParameterMode.IN, name = "eid", type = Integer.class),
 				@StoredProcedureParameter(mode = ParameterMode.OUT, name = "ename", type = String.class) }) })
 public class Employee {
@@ -26,6 +26,15 @@ public class Employee {
 	private int id;
 	private String name;
 	private int salary;
+
+	public Employee() {
+	}
+
+	public Employee(int id, String name, int salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
 
 	public int getId() {
 		return id;
--- employee-jpa/src/test/java/com/employee/jpa/poc/employeejpa/EmployeeJpaApplicationTests.java
+++ employee-jpa/src/test/java/com/employee/jpa/poc/employeejpa/EmployeeJpaApplicationTests.java
@@ -1,16 +1,105 @@
 package com.employee.jpa.poc.employeejpa;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poc.employeejpa.EmployeeJpaApplication;
+import com.poc.employeejpa.controller.EmployeeJpaController;
+import com.poc.employeejpa.entity.Employee;
+import com.poc.employeejpa.model.resp.EmployeeErrorResp;
+import com.poc.employeejpa.repository.EmployeeRespository;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = EmployeeJpaApplication.class)
 public class EmployeeJpaApplicationTests {
 
+	private MockMvc mockMvc;
+
+	@InjectMocks
+	private EmployeeJpaController empJpaController;
+
+	@Mock
+	private EmployeeRespository empRepository;
+
+	private ObjectMapper objMapper;
+
+	@Before
+	public void setup() {
+
+		mockMvc = MockMvcBuilders.standaloneSetup(empJpaController).build();
+		objMapper = new ObjectMapper();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void testGetAll() throws Exception {
+
+		when(empRepository.getAllEmployees())
+				.thenReturn(Arrays.asList(new Employee(1, "testName 1", 10000), new Employee(2, "testname 2", 2000)));
+		mockMvc.perform(get("/employeejpa/getAll")).andDo(print()).andExpect(status().is(200))
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+
+	}
+
+	@Test
+	public void testGetByEmpId() throws Exception {
+
+		when(empRepository.getEmpById(1)).thenReturn(Arrays.asList(new Employee(1, "testName 1", 10000)));
+		mockMvc.perform(get("/employeejpa/getEmpById").param("id", "1")).andDo(print()).andExpect(status().is(200))
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+
+	}
+
+	@Test
+	public void testGetByEmpIdAndSal() throws Exception {
+
+		when(empRepository.getEmpByIdAndSal(Mockito.anyInt(), Mockito.anyInt()))
+				.thenReturn(Arrays.asList(new Employee(1, "testName 1", 10000)));
+		mockMvc.perform(get("/employeejpa/getEmpByIdAndSal").param("id", "1").param("salary", "10000")).andDo(print())
+				.andExpect(status().is(200)).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+
+	}
+
+	@Test
+	public void testGetEmpNameById() throws Exception {
+
+		when(empRepository.getNameByEmpId(Mockito.anyInt())).thenReturn("Shivam");
+		mockMvc.perform(get("/employeejpa/getEmpNameById").param("id", "1")).andDo(print()).andExpect(status().is(200))
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+
+	}
+
+	@Test
+	public void testGetEmpNameByIdWith400Status() throws Exception {
+
+		when(empRepository.getNameByEmpId(Mockito.anyInt())).thenReturn(null);
+		MvcResult result = mockMvc.perform(get("/employeejpa/getEmpNameById").param("id", "1")).andDo(print())
+				.andExpect(status().is(400)).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andReturn();
+		EmployeeErrorResp employeeErrorResp = objMapper.readValue(new File("src/test/resources/errResp.json"),
+				EmployeeErrorResp.class);
+		String expResult = objMapper.writeValueAsString(employeeErrorResp);
+		JSONAssert.assertEquals(expResult, result.getResponse().getContentAsString(), true);
+
 	}
 
 }
